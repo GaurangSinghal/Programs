d205203077f7ea47d325f25f5f3e4a5c642b3d8c
GaurangSinghal <gaurang6singhal@gmail.com>
2017-03-10 22:50:22 +0530

    Add files via upload

---
+++ JAVA/Linked List/LinkedList.java
@@ -0,0 +1,49 @@
+
+package linked.list;
+
+import java.util.Scanner;
+
+public class LinkedList
+{
+    static Node start,end,temp;    
+    Node nd;
+    LinkedList()
+    {   start=null;
+        end=null;
+    }
+    
+    public static Node createNode(int val)
+    {   Node nd=new Node();
+        nd.key=val;
+        return nd;
+    }
+    
+    public static void insertNode(int val)
+    {   Node nd=createNode(val);        
+        if(start==null)
+        {   start=nd;
+            end=nd;            
+        }
+        else
+        {   temp=start;
+            start=nd;
+            start.link=temp;            
+        }
+    }
+    
+    public static void display(Node nd)
+    {   while(nd!=null)
+        {  System.out.print(nd.key+"-->");
+            nd=nd.link;
+        }
+    }
+    
+    public static void main(String[] args) 
+    {   Scanner sc=new Scanner(System.in);
+        for(int i=0;i<10;i++)
+        {   int val=sc.nextInt();
+            insertNode(val);    
+        }
+        display(start);
+    }    
+}
---
+++ JAVA/Linked List/Node.java
@@ -0,0 +1,12 @@
+
+package linked.list;
+
+public class Node
+{
+    Node link;
+    int key;
+    public Node()
+    {   link=null; 
+        key=0;
+    }    
+}
